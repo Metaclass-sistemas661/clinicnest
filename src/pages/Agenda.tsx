2f89218f7e077e12bb48d729ea7ee8cd20f8a22f
Andre27031510 <andre27031510@gmail.com>
2026-02-03 09:28:42 -0300

    initial commit

--- src/pages/Agenda.tsx
+++ src/pages/Agenda.tsx
@@ -357,13 +357,6 @@ export default function Agenda() {
     ).length;
   };
 
-  const formatCurrency = (value: number) => {
-    return new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    }).format(value);
-  };
-
   return (
     <MainLayout
       title="Agenda"
